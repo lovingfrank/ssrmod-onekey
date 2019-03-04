cfdeb1306697035574a70f7d9ac91d0fd7382e67
lovingfrank <lovingfrank@users.noreply.github.com>
2019-03-04 21:04:28 +0800

    fix ssr url

--- ssrmod-onekey-centos.sh
+++ ssrmod-onekey-centos.sh
@@ -37,7 +37,7 @@ yum -y install libffi-devel
 yum -y install openssl-devel
 echo ' ##########开始安装魔改后端###########'
 cd ~
-git clone -b manyuser https://github.com/glzjin/shadowsocks.git
+git clone -b manyuser https://github.com/NimaQu/shadowsocks.git
 cd shadowsocks
 pip install -r requirements.txt
 fi
